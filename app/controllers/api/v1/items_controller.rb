7cc53eaeb97e6c049f514b083f75d5ad1f6ce5a9
Springatom <Springatom@hotmail.com>
2024-07-15 01:41:16 +0800

    完成创建接口

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::ItemsController < ApplicationController
+  def create
+    amount = rand(1000001)
+    p amount
+    item = Item.new amount: amount
+    if item.save
+      render json: { resource: item }
+    else
+      render json: { errors: item.errors }
+    end
+  end
+
+  def show
+  end
 end
