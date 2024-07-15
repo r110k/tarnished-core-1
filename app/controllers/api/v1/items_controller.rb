7c310d45c3ada15b74720de9ad61e5dba756ffdc
Springatom <Springatom@hotmail.com>
2024-07-15 22:22:24 +0800

    使用 kanami 完成分页

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,6 +10,15 @@ class Api::V1::ItemsController < ApplicationController
     end
   end
 
-  def show
+  def index
+    items = Item.page params[:page]
+    render json: { 
+      resource: items,
+      pager: {
+        page: params[:page],
+        per_page: 200,
+	      total: Item.count
+      }
+    }
   end
 end
