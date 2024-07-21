82df745e33a95d8bd338c8cc3ac78cec261fc6d6
Springatom <Springatom@hotmail.com>
2024-07-21 11:33:41 +0800

    配置 rspec_api_documentation

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "验证码" do
+  header "Content-Type", "application/json"
+
   post '/api/v1/validation_codes' do
+    parameter :email, type: :string
+    let(:email) { 'xuxian@xihu.history' }
     example "请求发送验证码" do
       do_request
-
-      expect(status).to eq 422
+      expect(status).to eq 200
     end
   end
+
 end
