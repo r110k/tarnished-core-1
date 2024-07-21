f9736abde85616ebd212f09817ff3abf84ac16e5
Springatom <Springatom@hotmail.com>
2024-07-21 23:51:39 +0800

    时区可以统一成标准时区

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -3,15 +3,30 @@ require 'rails_helper'
 RSpec.describe "Items", type: :request do
   describe "获取账目" do
     it "按时间筛选" do
-      item1 = Item.create amount: 200000, created_at: Time.new(1991, 1, 2)
-      item2 = Item.create amount: 200000, created_at: Time.new(1991, 1, 2)
-      item3 = Item.create amount: 10000, created_at: Time.new(1992, 1, 1)
+      item1 = Item.create amount: 200000, created_at: '1991-1-2'
+      item2 = Item.create amount: 200000, created_at: '1991-1-2'
+      item3 = Item.create amount: 10000, created_at: '1992-1-1'
       get '/api/v1/items?created_after=1991-01-01&created_before=1991-1-3'
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 2
       expect(json['resources'][0]['id']).to eq item1.id
       expect(json['resources'][1]['id']).to eq item2.id
+    end
+
+    it "按时间筛选(边界条件)" do
+      # 这里(Time.new)默认使用了 +8 时区，会导致标准时区时间仍然在 1990-12-31 日，所以测试不通过
+      # item1 = Item.create amount: 200000, created_at: Time.new(1991, 1, 1) 
+      # 解决方案1, 指定标准时区（"+00:00" <=> "Z")
+      # item1 = Item.create amount: 200000, created_at: Time.new(1991, 1, 1, 0, 0, 0, "Z") 
+      # 解决方案2, 统一使用一个时区，使用字符串
+      item1 = Item.create amount: 200000, created_at: '1991-01-01'
+
+      get '/api/v1/items?created_after=1991-01-01&created_before=1991-1-2'
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq 1
+      expect(json['resources'][0]['id']).to eq item1.id
     end
 
     it "分页" do
