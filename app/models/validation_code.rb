ef0e4c0d07d79491861c9ea58a7f748d856cac0c
Springatom <Springatom@hotmail.com>
2024-07-21 16:58:56 +0800

    添加 validation_code 的 kind 枚举值

--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -1,8 +1,10 @@
 class ValidationCode < ApplicationRecord
   validates :email, presence: true
 
-  before_crete: :generate_code
-  after_create: :send_email
+  enum kind: { sign_in: 0, reset_password: 1 }
+
+  before_create :generate_code
+  after_create :send_email
 
   def generate_code
     self.code = SecureRandom.random_number.to_s[2..7]
