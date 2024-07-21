fb6b43a14b9c177be83f1230eafc6b0767c08ff7
Springatom <Springatom@hotmail.com>
2024-07-21 23:53:15 +0800

    调整测试用例顺序

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -2,6 +2,30 @@ require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
   describe "获取账目" do
+
+    it "分页" do
+      201.times { Item.create amount: rand(20000) }
+      expect(Item.count).to eq 201
+      get '/api/v1/items'
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq 100
+      get '/api/v1/items?page=3'
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq 1
+    end
+
+    it "创建账目" do
+      expect {
+        post '/api/v1/items', params: { amount: 777 }
+      }.to change { Item.count }.by +1
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['resource']['id']).to be_an(Numeric)
+      expect(json['resource']['amount']).to eq 777
+    end
+
     it "按时间筛选" do
       item1 = Item.create amount: 200000, created_at: '1991-1-2'
       item2 = Item.create amount: 200000, created_at: '1991-1-2'
@@ -27,29 +51,6 @@ RSpec.describe "Items", type: :request do
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
       expect(json['resources'][0]['id']).to eq item1.id
-    end
-
-    it "分页" do
-      201.times { Item.create amount: rand(20000) }
-      expect(Item.count).to eq 201
-      get '/api/v1/items'
-      expect(response).to have_http_status 200
-      json = JSON.parse(response.body)
-      expect(json['resources'].size).to eq 100
-      get '/api/v1/items?page=3'
-      expect(response).to have_http_status 200
-      json = JSON.parse(response.body)
-      expect(json['resources'].size).to eq 1
-    end
-
-    it "创建账目" do
-      expect {
-        post '/api/v1/items', params: { amount: 777 }
-      }.to change { Item.count }.by +1
-      expect(response).to have_http_status 200
-      json = JSON.parse(response.body)
-      expect(json['resource']['id']).to be_an(Numeric)
-      expect(json['resource']['amount']).to eq 777
     end
 
   end
