19a5262893abced7186c4b96420872a784134c25
Springatom <Springatom@hotmail.com>
2024-07-22 20:15:19 +0800

    完成创建标签 post /api/v1/tags

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,8 +1,22 @@
 class Api::V1::TagsController < ApplicationController
   def index
     current_user = User.find request.env['current_user_id']
-    return render status: :not_found if current_user.nil?
+    return render status: :unauthorized if current_user.nil?
     tags = Tag.where({ user_id: current_user.id }).page params[:page]
     render json: { resources: tags, pager: { page: params[:page] || 1, per_page: Tag.default_per_page, total: Tag.count } }
+  end
+
+  def create
+    current_user = User.find request.env['current_user_id']
+    return render status: :unauthorized if current_user.nil?
+    # 用下面的写法，感觉 create 的异常被吞掉了，返回的 tag 也没有 id
+    # tag = Tag.create name: params[:name], sign: params[:sign], user_id: current_user.id
+    # render json: { resource: tag }
+    tag = Tag.new name: params[:name], sign: params[:sign], user_id: current_user.id
+    if tag.save
+      render json: { resource: tag }
+    else
+      render json: { errors: tag.errors }, status: :unprocessable_entity
+    end
   end
 end
--- spec/requests/api/v1/tags_spec.rb
+++ spec/requests/api/v1/tags_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Tags", type: :request do
-  describe "标签" do
-
+  describe "获取标签" do
     it "测试不登陆获取标签接口会401" do
       get '/api/v1/tags'
       expect(response).to have_http_status :unauthorized
     end
 
-    it "获取标签" do
+    it "登陆后分页获取标签" do
       user = User.create email: 'judy@civilization.vi'
       11.times do |i| Tag.create name: "tag#{i}", sign: "sign#{i}", user_id: user.id end
       another_user = User.create email: 'tian@civilization.vi'
@@ -23,6 +22,38 @@ RSpec.describe "Tags", type: :request do
       expect(response).to have_http_status :ok
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
+    end
+  end
+
+  describe "创建标签" do
+    it "测试不登陆创建标签接口会401" do
+      post '/api/v1/tags'
+      expect(response).to have_http_status :unauthorized
+    end
+
+    it "登陆后创建标签" do
+      user = User.create email: 'judy@civilization.vi'
+      post '/api/v1/tags', params: {name: "tag_1", sign: "sign_1", user_id: user.id} ,headers: user.generate_auth_header
+      expect(response).to have_http_status :ok
+      json = JSON.parse(response.body)
+      expect(json['resource']['name']).to eq 'tag_1'
+      expect(json['resource']['sign']).to eq 'sign_1'
+    end
+
+    it "登陆后创建标签失败,因为没有填写name" do
+      user = User.create email: 'judy@civilization.vi'
+      post '/api/v1/tags', params: {sign: "sign_1", user_id: user.id} ,headers: user.generate_auth_header
+      expect(response).to have_http_status :unprocessable_entity
+      json = JSON.parse(response.body)
+      expect(json['errors']['name'][0]).to eq "can't be blank"
+    end
+
+    it "登陆后创建标签失败,因为没有填写sign" do
+      user = User.create email: 'judy@civilization.vi'
+      post '/api/v1/tags', params: {name: "tag_1", user_id: user.id} ,headers: user.generate_auth_header
+      expect(response).to have_http_status :unprocessable_entity
+      json = JSON.parse(response.body)
+      expect(json['errors']['sign'][0]).to eq "can't be blank"
     end
   end
 end
