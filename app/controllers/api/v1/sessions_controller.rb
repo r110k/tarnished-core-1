0b9c0a6b1fedab159a0631e38eb2603304bdbda8
Springatom <Springatom@hotmail.com>
2024-07-22 17:42:27 +0800

    一定可以登录成功

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,12 +9,8 @@ class Api::V1::SessionsController < ApplicationController
       # https://http.cat/401
       return render status: :unauthorized, json: { errors: '验证码错误' } unless canSignIn
     end
-    user = User.find_by_email params[:email]
-    if user.nil?
-      render status: :not_found, json: { errors: '邮箱有误' }
-    else
-      render status: :ok, json: { jwt: user.generate_jwt }
-    end
+    user = User.find_or_create_by email: params[:email]
+    render status: :ok, json: { jwt: user.generate_jwt }
   end
 
   def destroy
