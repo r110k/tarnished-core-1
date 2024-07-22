ff2a10301de49b643b4d9bb59afa8b616026e703
Springatom <Springatom@hotmail.com>
2024-07-22 09:44:27 +0800

    重构获取项目测试用例，添加鉴权

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -7,8 +7,8 @@ resource "账目" do
     parameter :created_after, '创建时间起点'
     parameter :created_before, '创建时间终点'
 
-    response_field :id, 'ID',scope: :resources
-    response_field :amount, '金额（单位：分）', scope: :resources
+    # response_field :id, 'ID',scope: :resources
+    # response_field :amount, '金额（单位：分）', scope: :resources
     # 上面两行等价于下面四行
     with_options :scope => :resources do 
       response_field :id, 'ID'
@@ -17,14 +17,23 @@ resource "账目" do
     let(:created_after) { '2019-12-31' }
     let(:created_before) { '2022-11-16' }
     example "获取账目" do
-      201.times do Item.create amount: rand(20000), created_at: '2020-1-1' end
-      expect(Item.count).to eq 201
+      user = User.create email: 'judy@civilization.vi'
+      11.times do Item.create amount: rand(20000), created_at: '2020-1-1', user_id: user.id end
+      expect(Item.count).to eq 11
 
+      jwt = ''
+      no_doc do
+        client.post '/api/v1/session', email: user.email, code: '926401'
+        json = JSON.parse response_body
+        jwt = json['jwt']
+      end
+
+      header 'Authorization', "Bearer #{jwt}"
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
       expect(json['resources'].size).to eq 10
-      expect(json['pager']['total']).to eq 201
+      expect(json['pager']['total']).to eq 11
     end
 
   end
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 RSpec.describe "Items", type: :request do
   describe "获取账目" do
 
+    it "创建账目" do
+      expect {
+        post '/api/v1/items', params: { amount: 777 }
+      }.to change { Item.count }.by +1
+      expect(response).to have_http_status 200
+      json = JSON.parse(response.body)
+      expect(json['resource']['id']).to be_an(Numeric)
+      expect(json['resource']['amount']).to eq 777
+    end
+
     it "测试不登陆调用分页接口会401" do
       11.times { Item.create amount: rand(200000) }
       expect(Item.count).to eq 11
@@ -34,21 +44,17 @@ RSpec.describe "Items", type: :request do
       expect(json['resources'].size).to eq 1
     end
 
-    it "创建账目" do
-      expect {
-        post '/api/v1/items', params: { amount: 777 }
-      }.to change { Item.count }.by +1
-      expect(response).to have_http_status 200
-      json = JSON.parse(response.body)
-      expect(json['resource']['id']).to be_an(Numeric)
-      expect(json['resource']['amount']).to eq 777
-    end
-
     it "按时间筛选" do
-      item1 = Item.create amount: 200000, created_at: '1991-1-2'
-      item2 = Item.create amount: 200000, created_at: '1991-1-2'
-      item3 = Item.create amount: 10000, created_at: '1992-1-1'
-      get '/api/v1/items?created_after=1991-01-01&created_before=1991-1-3'
+      user = User.create email: 'judy@civilization.vi'
+      item1 = Item.create amount: 200000, created_at: '1991-1-2', user_id: user.id
+      item2 = Item.create amount: 200000, created_at: '1991-1-2', user_id: user.id
+      item3 = Item.create amount: 10000, created_at: '1992-1-1', user_id: user.id
+
+      post '/api/v1/session', params: { email: user.email, code: '926401' }
+      data = JSON.parse response.body
+      jwt = data['jwt']
+
+      get '/api/v1/items?created_after=1991-01-01&created_before=1991-1-3', headers: { 'Authorization': "Bearer #{jwt}" }
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 2
@@ -57,14 +63,19 @@ RSpec.describe "Items", type: :request do
     end
 
     it "按时间筛选(边界条件)" do
+      user = User.create email: 'judy@civilization.vi'
       # 这里(Time.new)默认使用了 +8 时区，会导致标准时区时间仍然在 1990-12-31 日，所以测试不通过
       # item1 = Item.create amount: 200000, created_at: Time.new(1991, 1, 1) 
       # 解决方案1, 指定标准时区（"+00:00" <=> "Z")
       # item1 = Item.create amount: 200000, created_at: Time.new(1991, 1, 1, 0, 0, 0, "Z") 
       # 解决方案2, 统一使用一个时区，使用字符串
-      item1 = Item.create amount: 200000, created_at: '1991-01-01'
+      item1 = Item.create amount: 200000, created_at: '1991-01-01', user_id: user.id
 
-      get '/api/v1/items?created_after=1991-01-01&created_before=1991-1-2'
+      post '/api/v1/session', params: { email: user.email, code: '926401' }
+      data = JSON.parse response.body
+      jwt = data['jwt']
+
+      get '/api/v1/items?created_after=1991-01-01&created_before=1991-1-2', headers: { 'Authorization': "Bearer #{jwt}" }
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
@@ -72,10 +83,15 @@ RSpec.describe "Items", type: :request do
     end
 
     it "按时间筛选测试只传开始时间" do
-      item1 = Item.create amount: 200000, created_at: '1991-01-01'
-      item2 = Item.create amount: 200000, created_at: '1990-01-01'
+      user = User.create email: 'judy@civilization.vi'
+      item1 = Item.create amount: 200000, created_at: '1991-01-01', user_id: user.id
+      item2 = Item.create amount: 200000, created_at: '1990-01-01', user_id: user.id
 
-      get '/api/v1/items?created_after=1991-01-01'
+      post '/api/v1/session', params: { email: user.email, code: '926401' }
+      data = JSON.parse response.body
+      jwt = data['jwt']
+
+      get '/api/v1/items?created_after=1991-01-01', headers: { 'Authorization': "Bearer #{jwt}" }
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
@@ -83,10 +99,15 @@ RSpec.describe "Items", type: :request do
     end
 
     it "按时间筛选测试只传结束时间" do
-      item1 = Item.create amount: 200000, created_at: '1991-01-01'
-      item2 = Item.create amount: 200000, created_at: '1991-01-02'
+      user = User.create email: 'judy@civilization.vi'
+      item1 = Item.create amount: 200000, created_at: '1991-01-01', user_id: user.id
+      item2 = Item.create amount: 200000, created_at: '1991-01-02', user_id: user.id
 
-      get '/api/v1/items?created_before=1991-01-01'
+      post '/api/v1/session', params: { email: user.email, code: '926401' }
+      data = JSON.parse response.body
+      jwt = data['jwt']
+
+      get '/api/v1/items?created_before=1991-01-01', headers: { 'Authorization': "Bearer #{jwt}" }
       expect(response).to have_http_status 200
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq 1
