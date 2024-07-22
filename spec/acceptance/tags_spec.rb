4778d1cf272be94c15660566d97e50a863f1dd08
Springatom <Springatom@hotmail.com>
2024-07-22 22:12:44 +0800

    编写更新、创建tag的文档

--- spec/acceptance/tags_spec.rb
+++ spec/acceptance/tags_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "标签" do
+  authentication :basic, :auth
+  let(:current_user) { User.create email: 'judy@civilization.vi' }
+  let(:auth) { "Bearer #{current_user.generate_jwt}" }
+
   get '/api/v1/tags' do
-    authentication :basic, :auth
     parameter :page, '页码'
 
     with_options :scope => :resources do
@@ -14,14 +17,64 @@ resource "标签" do
       response_field :deleted_at, '删除时间'
     end
    
-    let(:current_user) { User.create email: 'judy@civilization.vi' }
-    let(:auth) { "Bearer #{current_user.generate_jwt}" }
     example "获取标签" do
       11.times do |i| Tag.create name: "tag#{i}", sign: "sign#{i}", user_id: current_user.id end
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
       expect(json['resources'].size).to eq 10
+    end
+
+  end
+
+  post '/api/v1/tags' do
+    parameter :name, '名称', required: true
+    parameter :sign, '符号', required: true
+
+    with_options :scope => :resources do
+      response_field :id, 'ID'
+      response_field :name, '名称'
+      response_field :sign, '符号'
+      response_field :user_id, '用户 ID'
+      response_field :deleted_at, '删除时间'
+    end
+   
+    let (:name) { 'tag_1' }
+    let (:sign) { 'sign_1' }
+    example "创建标签" do
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json['resource']['name']).to eq name
+      expect(json['resource']['sign']).to eq sign
+      expect(json['resource']['user_id']).to eq current_user.id
+    end
+  end
+
+  patch '/api/v1/tags/:id' do
+    let(:tag) { Tag.create name: 'tag_1', sign: 'sign_1', user_id: current_user.id }
+    let(:id) { tag.id }
+
+    parameter :name, '名称'
+    parameter :sign, '符号'
+
+    with_options :scope => :resources do
+      response_field :id, 'ID'
+      response_field :name, '名称'
+      response_field :sign, '符号'
+      response_field :user_id, '用户 ID'
+      response_field :deleted_at, '删除时间'
+    end
+   
+    let (:name) { 'tag_so' }
+    let (:sign) { 'sign_so' }
+
+    example "更新标签" do
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json['resource']['name']).to eq name
+      expect(json['resource']['sign']).to eq sign
     end
 
   end
