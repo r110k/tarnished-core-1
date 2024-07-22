3723e3c45e66df0d309fe8f7eef8e01ed3a6cb09
Springatom <Springatom@hotmail.com>
2024-07-22 14:51:31 +0800

    完成 jwt 的过期检查

--- spec/requests/api/v1/mes_spec.rb
+++ spec/requests/api/v1/mes_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
-
+require 'active_support/testing/time_helpers'
 
 RSpec.describe "Mes", type: :request do
+  include ActiveSupport::Testing::TimeHelpers
   describe "当前用户" do
     it "成功登录后能被获取" do
       # 登录
@@ -15,6 +16,30 @@ RSpec.describe "Mes", type: :request do
       expect(response).to have_http_status(:ok)
       data = JSON.parse response.body
       expect(data['resource']['id']).to eq user.id
+    end
+
+    it "jwt可以过期" do
+      travel_to Time.now - 3.hours
+      user = User.create email: 'judy@civilization.vi'
+      jwt = user.generate_jwt
+
+      travel_back
+      get '/api/v1/me', headers: { 'Authorization': "Bearer #{jwt}" }
+      p 'response ----------------------------------------'
+      p response
+      expect(response).to have_http_status :unauthorized
+    end
+
+    it "jwt没过期" do
+      travel_to Time.now - 1.hours
+      user = User.create email: 'judy@civilization.vi'
+      jwt = user.generate_jwt
+
+      travel_back
+      get '/api/v1/me', headers: { 'Authorization': "Bearer #{jwt}" }
+      p 'response ----------------------------------------'
+      p response
+      expect(response).to have_http_status :ok
     end
   end
 end
