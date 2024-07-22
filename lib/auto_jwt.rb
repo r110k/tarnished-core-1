ce63fc81d932601424919ca605f5cd5cf31b52ac
Springatom <Springatom@hotmail.com>
2024-07-22 14:33:17 +0800

    使用 auto_jwt 拦截所有未鉴权的请求，并配置白名单

--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -6,11 +6,21 @@ class AutoJwt
   def call(env)
     #  状态码，响应头，响应体, 如果你写了下面一行代码， controller 里面的逻辑将不会再执行，会直接返回
     # [200, {}, ['Hello, World!', 'Tarnished!']]
+
+    # jwt 跳过白名单
+    return @app.call(env) if ['/api/v1/session'].include? env['PATH_INFO']
+
     header = env['HTTP_AUTHORIZATION']
     # Bearer jwt
     jwt = header.split(' ')[1] rescue ''
-    # payload ex: [{"user_id"=>24}, {"alg"=>"HS256"}]
-    payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } rescue nil
+    begin
+      # payload ex: [{"user_id"=>24}, {"alg"=>"HS256"}]
+      payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' }
+    rescue JWT::ExpiredSignature
+      return [401, {}, [JSON.generate({ reason: 'token 失效' })]]
+    rescue
+      return [401, {}, [JSON.generate({ reason: 'token 非法' })]]
+    end
     env['current_user_id'] = payload[0]['user_id'] rescue nil
     @status, @headers, @response = @app.call(env)
     [@status, @headers, @response]
