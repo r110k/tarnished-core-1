efcc15d86700ec2102a89f52a85a3a2af7018979
Springatom <Springatom@hotmail.com>
2024-07-22 15:18:20 +0800

    完成登录 api 文档

---
+++ spec/acceptance/sessions_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "会话" do
+  post '/api/v1/session' do
+    parameter :email, '邮箱', required: true
+    parameter :code, '验证码', required: true
+
+    response_field :jwt, '用于验证用户身份的 token'
+
+    let(:email) { 'judy@civilization.vi' }
+    let(:code) { '926401' }
+
+    example "登录" do
+      User.create email: email
+      expect(User.count).to eq 1
+
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json['jwt']).to be_a String
+    end
+
+  end
+end
