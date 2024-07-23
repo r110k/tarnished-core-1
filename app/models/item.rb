52cc6c6ad6d915fe91d284b8f36e4eee9c667708
Springatom <Springatom@hotmail.com>
2024-07-23 19:17:57 +0800

    新增 migration 给 item 增加 kind 记录收入/指出

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,3 @@
 class Item < ApplicationRecord
+  enum kind: { income: 1, expenses: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_22_110135) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_23_111607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_22_110135) do
     t.bigint "tags_id", array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "kind", default: 1, null: false
   end
 
   create_table "tags", force: :cascade do |t|
